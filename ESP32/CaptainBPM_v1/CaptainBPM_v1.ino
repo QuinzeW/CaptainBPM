83d40c36b5a2a27e33115e0955d336cc66cca75f
QuinzeW <maxime.lefeuvre@gmail.com>
2026-05-14 15:36:29 +0200

    v1.056

--- ESP32/CaptainBPM_v1/CaptainBPM_v1.ino
+++ ESP32/CaptainBPM_v1/CaptainBPM_v1.ino
@@ -4,7 +4,7 @@
 
 // MIDI BLE OFFICIEL
 #define MIDI_SERVICE_UUID "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
-#define MIDI_IO_UUID      "7772e5db-3868-4112-a1a9-f2669d106bf3"
+#define MIDI_IO_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
 
 #define BTN_UP 23
 #define BTN_DOWN 21
